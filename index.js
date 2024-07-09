f3897f5865d50b1deb166190b4ac0534435d7b4b
vishnudeviravi <ivishnudr@gmail.com>
2024-07-09 15:47:11 +0530

    ADDING COMMENTS

--- index.js
+++ index.js
@@ -5,3 +5,14 @@ const app = express();
 app.listen(8888, () => {
   console.log('app is running');
 });
+// status  git status
+
+// git add .
+
+// git commit -m "MESSAGE"
+
+//git remote add origin link
+
+// git branch -M
+
+// git push origin main
